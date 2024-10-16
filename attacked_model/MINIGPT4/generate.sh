0cc4e6af2e7a0417b9ba03dfb39aa91f9aa18791
YeRookie <119864930+YeRookie@users.noreply.github.com>
2024-10-16 15:39:50 +0800

    Update generate.sh

--- attacked_model/MINIGPT4/generate.sh
+++ attacked_model/MINIGPT4/generate.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=4
-export TORCH_HOME=/home/dycpu6_8tssd1/jmzhang/.cache/
+#export CUDA_VISIBLE_DEVICES=0
+export TORCH_HOME=/home/.cache/
+# Define base paths through environment variables
+export PROJECT_PATH="your_project_path"
+dataset="coco_caption"
+method="anyattack"
 
-methods=("en_Cosine")
-# losses = ["Cosine"]
+python generate_response.py  \
+    --cfg_path 'minigpt4_llama2_eval.yaml' \
+    --prompt "Describe this image in one short sentence only." \
+    --data_path "${PROJECT_PATH}/json/${dataset}_adv.json" \
+    --image_path  "${PROJECT_PATH}/outputs/${dataset}/${method}" \
+    --gt_path "${PROJECT_PATH}/json/coco_caption_test_gt_adv.json" \
+    --output_path "${PROJECT_PATH}/outputs/minigpt4-llama2/${method}.json" \
+    --llama_path 'meta-llama/Llama-2-7b-chat-hf' \
+    --ckpt_path '/YOUR/CHECKPOINT/PATH'
 
-#     --image_path '/home/dycpu6_8tssd1/jmzhang/codes/text_guided_attack/outputs/coco_caption/early_Cosine' \
-#    --data_path '/home/dycpu6_8tssd1/jmzhang/codes/text_guided_attack/outputs/coco_caption_early_Cosine.json' \
-# --image_path '/home/dycpu6_8tssd1/jmzhang/datasets/mscoco' \
-# --data_path '/home/dycpu6_8tssd1/jmzhang/codes/text_guided_attack/json/coco_karpathy_test.json' \
+python generate_response.py  \
+    --cfg_path 'minigpt4v2_llama2_eval.yaml' \
+    --prompt "Describe this image in one short sentence only." \
+    --data_path "${PROJECT_PATH}/json/${dataset}_adv.json" \
+    --image_path  "${PROJECT_PATH}/outputs/${dataset}/${method}" \
+    --gt_path "${PROJECT_PATH}/json/coco_caption_test_gt_adv.json" \
+    --output_path "${PROJECT_PATH}/output/minigpt4v2-llama2/${method}.json" \
+    --llama_path 'meta-llama/Llama-2-7b-chat-hf' \
+    --ckpt_path '/YOUR/CHECKPOINT/PATH'
 
 
-for m in "${methods[@]}"; do
-  python generate_response.py  \
-      --cfg_path 'minigpt4v2_llama2_eval.yaml' \
-      --prompt "Describe this image in one short sentence only." \
-      --data_path '/home/dycpu6_8tssd1/jmzhang/codes/text_guided_attack/outputs/coco_caption_early_Cosine.json' \
-      --image_path "/home/dycpu6_8tssd1/jmzhang/codes/text_guided_attack/outputs/coco_caption/${m}" \
-      --gt_path '/home/dycpu6_8tssd1/jmzhang/codes/text_guided_attack/json/coco_caption_test_gt_adv.json' \
-      --output_path "output/minigpt4V2-llama2/${m}.json" \
-      --llama_path 'meta-llama/Llama-2-7b-chat-hf' \
-      --ckpt_path '/home/dycpu6_8tssd1/jmzhang/codes/text_guided_attack/checkpoints/minigptv2_checkpoint.pth'
-done
 
-#for m in "${methods[@]}"; do
-#  python generate_response.py  \
-#    --cfg_path 'minigpt4_llama2_eval.yaml' \
-#    --prompt "Describe this image in one short sentence only." \
-#    --data_path '/home/dycpu6_8tssd1/jmzhang/codes/text_guided_attack/outputs/coco_caption_early_Cosine.json' \
-#    --image_path "/home/dycpu6_8tssd1/jmzhang/codes/text_guided_attack/outputs/coco_caption/${m}" \
-#    --gt_path '/home/dycpu6_8tssd1/jmzhang/codes/text_guided_attack/json/coco_caption_test_gt_adv.json' \
-#    --output_path "output/minigpt4-llama2/${m}.json" \
-#    --llama_path 'meta-llama/Llama-2-7b-chat-hf' \
-#    --ckpt_path '/home/dycpu6_8tssd1/jmzhang/codes/text_guided_attack/checkpoints/pretrained_minigpt4_llama2_7b.pth'
-#done
